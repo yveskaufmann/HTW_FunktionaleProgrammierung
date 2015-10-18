52119f2f6892c3981b6a8d19daf9ca552f9c8236
Yves Kaufmann <fxdapokalypse@googlemail.com>
2015-10-18 02:10:59 +0200

    Updates the third exercise:
    [X] Task 2: check if a number exists in a list
    [X] Task 3: sort a number into a sorted list
    [X] Task 4: calculate the checksum
    [X] Task 5: sort an unsorted list

--- u3.erl
+++ u3.erl
@@ -2,7 +2,8 @@
 -export([
     member/2,
     sel_sort/2,
-    checksum/1
+    checksum/1,
+	sort/1
 ]).
 
 % Check if a Number exists inside a list
@@ -11,11 +12,18 @@ member(X, [_|[X|_]]) -> true;
 member(X, [_|X]) -> true;
 member(_, _) -> false.
 
+% Calcualtes the checksum of a given number.
+checksum(0) -> 0;
+checksum(N) -> N rem 10 + checksum(N div 10).
+
 % Sort a number into a sorted list
 sel_sort(X, L) -> sel_sort(sort_in,[],[X|L]).
 sel_sort(sort_in, H,[X,N|T]) when X > N -> sel_sort(sort_in, H ++ [N],[X|T]);
 sel_sort(sort_in, H, L) -> H ++ L.
 
-% Calcualtes the checksum of a given number.
-checksum(0) -> 0;
-checksum(N) -> N rem 10 + checksum(N div 10).
+% Sorts an unsorted list
+sort(L) -> lists:foldl(fun sel_sort/2, [], L).
+
+
+
+
