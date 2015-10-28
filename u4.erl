c4a424513ab0e5edb61ef918c42614d3ce0d3bb7
Yves Kaufmann <fxdapokalypse@googlemail.com>
2015-10-28 04:04:41 +0100

    Add a binary traverse strategy creator

--- u4.erl
+++ u4.erl
@@ -3,7 +3,10 @@
 -export([
 	create_tree/1,
 	insert/3,
-	height/1
+	height/1,
+	traverse_in_order/2,
+	traverse_in_depth/2,
+	create_traverse/1
 ]).
 % Check if a Number exists inside a list
 
@@ -17,8 +20,29 @@ insert(Key, Value, Node) ->
 
 
 create_tree([]) -> nil;
-create_tree([X|XS]) -> insert(X, 0, create_tree(XS)).
+create_tree([X|XS]) -> insert(X, X, create_tree(XS)).
 
-height(Node) -> round(math:log2(height_r(Node))).
-height_r(nil) -> 0;
-height_r(Node) -> height_r(Node#node.left) + height_r(Node#node.right) + 1.
+height(Node) -> round(math:log2(height_r(Node, 0))).
+height_r(nil, N) -> N;
+height_r(Node, N) -> height_r(Node#node.left, 1 + height_r(Node#node.right, N)).
+
+strategie(n) -> 
+	fun(Node, Fun, _) -> apply(Fun, [Node#node.value]) end;
+strategie(l) -> 
+	fun(Node, Fun, Traverse) -> apply(Traverse, [Node#node.left, Fun]) end;
+strategie(r) -> 
+	fun(Node, Fun, Traverse) -> apply(Traverse,[Node#node.right, Fun]) end.
+
+create_traverse([S1, S2, S3]) -> 
+	Strategie = [strategie(S1), strategie(S2), strategie(S3)],
+	fun Traverse(Node, Visitor) ->
+		case Node of
+			nil -> ok;
+			_ -> lists:foreach(
+				fun(S) -> 
+					apply(S, [Node, Visitor, Traverse]) 
+				end, Strategie)			
+		end
+	end.
+
+
