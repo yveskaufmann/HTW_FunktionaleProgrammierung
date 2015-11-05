8cca2b0dfcc4b76a07e42f798273d21f19dfa693
Yves Kaufmann <fxdapokalypse@googlemail.com>
2015-11-05 22:26:11 +0100

    Add the first assigment task

---
+++ Beleg1/bel1.erl
@@ -0,0 +1,132 @@
+-module(bel1).
+-compile(export_all).
+%-export([encode/2, decode/2, createCodeTree/1]).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Ein Huffman Code wird durch einen Binaermaum repraesentiert.
+%
+%  Jedes Blatt beinhaltet ein Zeichen, das durch den Baum kodiert wird.
+%  Das Gewicht entspricht der Haeufigkeit des Vorkommens eines Zeichen innerhalb eines Texts.
+%    
+%  Die inneren Knoten repraesentieren die Kodierung. Die assoziierten Zeichen weisen auf die 
+%  darunter liegenden Blaetter. Das Gewicht entspricht der Summe aller Zeichen, die darunter liegen.
+% 
+%
+% Definition of the Tree: two kinds of nodes:
+% fork - representing the inner nodes (binary tree)
+% leaf - representing the leafs of the tree
+%
+-type tree():: fork() | leaf().
+
+-record(fork, {left::tree(), right::tree(), chars::list(char()), weight::non_neg_integer()}).
+-type fork() :: #fork{}.
+-record(leaf, {char::char(), weight::non_neg_integer()}).
+-type leaf() :: #leaf{}.
+-type bit() :: 0 | 1. 
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Basisfunktionen
+-spec weight(tree()) -> non_neg_integer().
+weight(#fork{weight=W}) -> W;
+weight(#leaf{weight=W}) -> W.
+
+-spec chars(tree()) -> list(char()).
+chars(#fork{chars=C}) -> C;
+chars(#leaf{char=C}) -> [C].
+
+% Erzeugung eines CodeTrees aus zwei Teilbaeumen
+% Aus Gruenden der Testbarkeit werden links die Teilbaeume mit dem alphabetisch kleinerem Wert der 
+% Zeichenketten angeordnet. 
+-spec makeCodeTree( T1::tree(), T2::tree()) -> tree().
+makeCodeTree(T1 , T2) -> toBeDefined.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%    Erzeugung eines Huffman Trees
+%
+%   Schreiben Sie eine Funktion createFrequencies, die aus einem Text die Haeufigkeiten des Vorkommens
+%   eines Zeichen in der Zeichenkette berechnet.
+% 
+%  Ergebnis der Funktion soll eine Liste von Zweiertupeln sein, die als erstes Element den Character und als 
+%  zweites die Haeufigkeit enthaelt.
+%
+%  createFrequencies("Dies ist ein Test") waere also [{$D,1}, {$i,3}, {$e,3}, {$s, 3}, {$ , 3}, {$t, 2}, {$n, 1}, {$T,1}] 
+%  
+%  Auf die Elemente eines Tupels kann ueber Pattern Matching zugegriffen werden: 
+%  z.B. {X,Y} = {$a,4}
+%  Tipp: Splitten Sie die Funktion auf:
+%  1. Funktion zum Eingliedern des Buchstabens in die Tupelliste (z.B. addLetter(...))
+%  2. Aufruf der Funktion fuer jeden Buchstaben
+
+-spec addLetter(list({char(),non_neg_integer()}), char())-> list({char(), non_neg_integer()}).
+addLetter(TupelList, Char) -> toBeDefined.
+
+-spec createFrequencies(list(char())) -> list({char(), non_neg_integer()}).
+createFrequencies(Text) -> toBeDefined.
+
+%  Erzeugung eines Blattknotens fuer jeden Buchstaben in der Liste
+%  Aufsteigendes Sortieren der Blattknoten nach den Haeufigkeiten der Vorkommen der Buchstaben
+%  z.B. aus makeOrderedLeafList([{$b,5},{$d,2},{$e,11},{$a,7}])
+% wird [#leaf{char=$d,weight=2},#leaf{char=$b,weight=5},#leaf{char=$a,weight=7},#leaf{char=$e,weight=11}]
+-spec makeOrderedLeafList(FreqList::list({char(), non_neg_integer()})) -> list(leaf()).
+makeOrderedLeafList(FeqList) -> toBeDefined.
+
+%  Bei jedem Aufruf von combine sollen immer zwei Teilbaeume (egal ob fork oder leaf) zusammenfuegt werden.
+%  Der Parameter der Funktion combine ist eine aufsteigend sortierte Liste von Knoten.
+%
+%  Die Funktion soll die ersten beiden Elemente der Liste nehmen, die Baeume zusammenfuegen
+%  und den neuen Knoten wieder in die Liste einfuegen sowie die zusammengefuegten aus der Liste 
+%  loeschen. Dabei sollen der neue Knoten so eingefuegt werden, dass wieder eine sortierte Liste von
+%  Knoten entsteht.
+%
+%  Ergebnis der Funktion soll wiederum eine sortierte Liste von Knoten sein.
+% 
+%  Hat die Funktion weniger als zwei Elemente, so soll die Liste unveraendert bleiben.
+%  Achtung: Ob die vorgefertigten Tests funktionieren, haengt davon ab, auf welcher Seite die Knoten
+%  eingefuegt werden. Die Tests sind genau dann erfolgreich, wenn Sie die Baeume so kombinieren, dass 
+%  ein Baum entsteht, der so angeordnet ist, wie im Beispiel auf dem Aufgabenblatt. Sorgen Sie dafuer,
+%  dass die Teilbaeume ebenso eingefuegt werden (erhoehter Schwierigkeitsgrad) oder schreiben Sie eigene
+%  Tests. 
+
+-spec combine(list(tree())) -> list(tree()).		
+combine([TreeList]) -> toBeDefined.
+
+%  Die Funktion repeatCombine soll die Funktion combine so lange aufrufen, bis nur noch ein Gesamtbaum uebrig ist.		
+-spec repeatCombine(TreeList::list(tree())) -> tree().
+repeatCombine(TreeList)-> toBeDefined.
+
+%  createCodeTree fuegt die einzelnen Teilfunktionen zusammen. Soll aus einem gegebenen Text, den Gesamtbaum erzeugen.
+-spec createCodeTree(Text::list(char())) -> tree().
+createCodeTree(Text)-> toBeDefined.
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+% Dekodieren einer Bitsequenz
+%
+% Die Funktion decode soll eine Liste von Bits mit einem gegebenen Huffman Code (CodeTree) dekodieren.
+% Ergebnis soll die Zeichenkette im Klartext sein.	
+-spec decode(CodeTree::tree(), list( bit())) -> list(char()).
+decode(CodeTree, BitList) -> toBeDefined.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+%  Kodieren einer Bitsequenz
+%
+%  Die Funktion encode soll eine Liste von Bits mit einem gegebenen Huffman Code (CodeTree) kodieren.
+%  Ergebnis soll die Bitsequenz sein.
+%
+%  Gehen Sie dabei folgendermassen vor:
+%  Schreiben Sie eine Funktion convert, die aus einem Codetree eine Tabelle generiert, die fuer jeden 
+%  Buchstaben die jeweilige Bitsequenz bereitstellt. Dabei soll jeder Eintrag ein Tupel sein bestehend
+%  aus dem Character und der Bitsequenz.
+%  Also: convert(CodeTree)->[{Char,BitSeq},...]
+-spec convert(CodeTree::tree()) -> list({char(), list(bit())}).
+convert(CodeTree) -> toBeDefined.
+
+%  Schreiben Sie eine Funktion encode, die aus einem Text und einem CodeTree die entsprechende 
+%  Bitsequenz generiert.
+%  Verwenden Sie dabei die erzeugte Tabelle.
+-spec encode(Text::list(char()), CodeTree::tree()) -> list(bit()).
+encode(Text, CodeTree) -> toBeDefined.
+
---
+++ Beleg1/bel1_tests.erl
@@ -0,0 +1,92 @@
+-module(bel1_tests).
+-include_lib("eunit/include/eunit.hrl").
+-record(fork, {left, right, chars, weight}).
+-record(leaf, {char, weight}).
+
+% Creating some trees for testing
+createTree0() -> #leaf{char=$d,weight=4}.
+createTree1() -> #fork{left=#leaf{char=$a, weight=2},right=#leaf{char=$b, weight=3},chars=[$a,$b],weight=5}.
+createTree2() -> #fork{left=#fork{left=#leaf{char=$a, weight=2},right=#leaf{char=$b, weight=3},chars=[$a,$b],weight=5},
+						right=#leaf{char=$d, weight=4}, chars=[$a,$b,$d], weight=9}.
+
+% Example Tree from the assignment description
+exampleTree() -> #fork{ left = #leaf{char = 65,weight = 8}, right = #fork{left = #fork{
+             left = #leaf{char = 66,weight = 3}, right = #fork{ left = #leaf{char = 67,weight = 1},
+             right = #leaf{char = 68,weight = 1}, chars = "CD",weight = 2}, chars = "BCD",weight = 5},
+             right = #fork{ left = #fork{ left = #leaf{char = 69,weight = 1},right = #leaf{char = 70,weight = 1},
+             chars = "EF",weight = 2},right = #fork{ left = #leaf{char = 71,weight = 1}, right = #leaf{char = 72,weight = 1},
+             chars = "GH",weight = 2}, chars = "EFGH",weight = 4}, chars = "BCDEFGH",weight = 9}, chars = "ABCDEFGH",weight = 17}.
+exampleString()-> "AAAABCDEFGHAAAABB".
+				
+% tests of the basic functions						
+weight_test_()->
+	[	{setup, fun createTree0/0, fun(X)->?_assertEqual(4,bel1:weight(X)) end},
+		{setup, fun createTree1/0, fun(X)->?_assertEqual(5,bel1:weight(X)) end},
+		{setup, fun createTree2/0, fun(X)->?_assertEqual(9,bel1:weight(X)) end}].
+
+char_test_()->
+	[	{setup, fun createTree0/0, fun(X)->?_assertEqual([$d],bel1:chars(X)) end},
+		{setup, fun createTree1/0, fun(X)->?_assertEqual([$a,$b],bel1:chars(X)) end},
+		{setup, fun createTree2/0, fun(X)->?_assertEqual([$a,$b,$d],bel1:chars(X)) end}].
+
+char2_test_()->
+	[	{setup, fun createTree0/0, fun(X)->?_assertEqual([$d],bel1:chars(X)) end},
+		{setup, fun createTree1/0, fun(X)->?_assertEqual([$a,$b],bel1:chars(X)) end},
+		{setup, fun createTree2/0, fun(X)->?_assertEqual([$a,$b,$d],bel1:chars(X)) end}].
+
+mTree_test_()->
+		?_assertEqual(createTree2(),bel1:makeCodeTree(createTree1(),createTree0())).
+
+% test for creating Huffman Trees
+
+addLetter_test_() ->
+	[	?_assertEqual([{$a,1}], bel1:addLetter([],$a)),
+		?_assertEqual([{$a,1}], bel1:addLetter([],$a)),
+		?_assertMatch([{_,2},{_,2},{_,2}], bel1:addLetter([{$a,2},{$b,1},{$c,2}],$b))
+	].
+	
+createFrequency_test_()-> 
+	?_assertEqual([{$a,2},{$b,3},{$c,2},{$d,1},{$q,1}], lists:sort( fun({X,_},{Y,_})->X<Y end,
+								bel1:createFrequencies("abqcabcdb"))).
+
+makeOrderedLeafList_test_()->
+	?_assertEqual([#leaf{char=$d,weight=2},#leaf{char=$b,weight=5},#leaf{char=$a,weight=7},#leaf{char=$e,weight=11}],
+		bel1:makeOrderedLeafList([{$b,5},{$d,2},{$e,11},{$a,7}])).
+
+combine_test_ignore() ->
+	[
+		?_assertEqual([],bel1:combine([])),
+		?_assertEqual([#leaf{char=$a, weight=2}],bel1:combine([#leaf{char=$a, weight=2}])),
+		?_assertEqual([createTree1()],bel1:combine([#leaf{char=$a, weight=2},#leaf{char=$b, weight=3}])),
+		?_assertEqual([#leaf{char = 99,weight = 7}, #leaf{char = 100,weight = 8},
+						#fork{left = #leaf{char = 97,weight = 5},right = #leaf{char = 98,weight = 6},
+						chars = "ab",weight = 11},#leaf{char = 101,weight = 14}],
+						bel1:combine([#leaf{char=$a, weight=5},#leaf{char=$b, weight=6},#leaf{char=$c, weight=7},
+									#leaf{char=$d, weight=8},#leaf{char=$e, weight=14}])),
+		?_assertEqual([#fork{left = #leaf{char = 98,weight = 5},right = #leaf{char = 100,weight = 2},
+					chars = "bd",weight = 7},#leaf{char=$a, weight=7},#leaf{char=$e,weight=11}],
+					bel1:combine([#leaf{char=$d,weight=2},#leaf{char=$b,weight=5},
+					#leaf{char=$a,weight=7},#leaf{char=$e,weight=11}]))
+	].
+
+repeatCombine_test_ignore()->
+	?_assertEqual({fork, {fork, {leaf,97,7}, {fork,{leaf,98,5},{leaf,100,2},"bd",7}, "abd",14}, {leaf,101,11}, "abde",25},
+	bel1:repeatCombine([#leaf{char=$d,weight=2},#leaf{char=$b,weight=5},#leaf{char=$a,weight=7},
+		#leaf{char=$e,weight=11}])).
+
+createCodeTree_test_ignore()->
+	[
+		?_assertEqual( {fork, {fork,{leaf,$a,7},{fork,{leaf, $b,5},{leaf,$d,2},"bd",7},"abd",14},{leaf,$e,11},"abde",25},
+			bel1:createCodeTree("eeedeeedeeeeebbbbbaaaaaaa")),
+		?_assertEqual(exampleTree(),
+			bel1:createCodeTree(exampleString()))
+	].
+
+decode_test_()->
+	?_assertEqual(
+		"BAAHGA",
+		bel1:decode(exampleTree(),[1,0,0,0,0,1,1,1,1,1,1,1,0,0])).
+
+encode_decode_test_()->
+		?_assertEqual("ADDABHGACDABGHAAAA", bel1:decode(exampleTree(),bel1:encode("ADDABHGACDABGHAAAA",exampleTree()))).
+
